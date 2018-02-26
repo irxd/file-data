cf8274718c1885630909baaa70f76d5ce3097932
Glitch (filemd) <none>
2018-02-26 09:43:22 +0000

    🌈🐒 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var multer = require('multer');
+var upload = multer();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post("/upload", upload.single('upload'), function (request, response) {
+  response.json({"size": request.file["size"] });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
